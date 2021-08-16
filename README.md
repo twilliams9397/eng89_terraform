4ab85ae5a98e30763d248bad5f6245b3406901d4
twilliams9397 <twilliams@spartaglobal.com>
2021-08-16 10:54:10 +0100

    updates

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 - main.tf contains instructions
 - set AWS access keys as environment variables - AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
 - there are other options - secret.txt, vaults, .gitignore - but variables is most secure
+- put .terraform/ in the .gitignore as it gets large and heavy to push every time
+- terraform gathers dependencies when it is initialised with `terraform init` using the `main.tf` instructions
--- main.tf
+++ main.tf
@@ -1,8 +1,26 @@
 # let's build a script to connect to AWS and download/setup all dependencies
-# keyword: provider aws 
+# keyword: provider -> aws 
+
 provider "aws" {
 	region = "eu-west-1"
 }
 
-# then we will run terraform init (to initialise)
+# then we will run terraform init (to initialise) in terminal/gitbash
 # then we will move on to launch aws services
+
+# lets launch an ec2 instance in eu-west-1 with ami-038d7b856fe7557b3 - ubuntu 16.04
+# keyword: resource -> provide resource name and give specific details to service
+# aws_ec2_instance, name, ami, type of instance, with/without ip - tags is keyword to name it
+
+resource "aws_instance" "app_instance" {
+	ami = "ami-038d7b856fe7557b3"
+	instance_type = "t2.micro"
+	associate_public_ip = true
+	tags = {
+		Name = "eng89_tom_terraform"
+	}
+
+}
+
+# terraform plan checks syntax and validates instructions
+# when outcome is all green, run terraform apply to run the script
