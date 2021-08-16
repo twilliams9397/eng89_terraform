b9048f7b4601d36f9590bb3b5f1a48d71686e711
twilliams9397 <twilliams@spartaglobal.com>
2021-08-16 10:36:34 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
 # let's build a script to connect to AWS and download/setup all dependencies
 # keyword: provider aws 
+provider "aws" {
+	region = "eu-west-1"
+}
+
 # then we will run terraform init (to initialise)
 # then we will move on to launch aws services
